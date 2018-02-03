114211707e4dadb0e8aeb36bc6f44bdbf89c42b9
ALEXANDRE Julien <22328659+ndrpnt@users.noreply.github.com>
2018-02-03 16:34:25 +0100

    Clean up path.join calls

--- server/src/index.js
+++ server/src/index.js
@@ -4,11 +4,9 @@ import path from 'path'
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '/../../client/build')))
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../../client/build/index.html'))
-})
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html')))
 
 const port = process.env.PORT || 5000
 app.listen(port)
