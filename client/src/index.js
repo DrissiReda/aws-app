159893f9523db5241a882a29f01875db0883e55e
ALEXANDRE Julien <22328659+ndrpnt@users.noreply.github.com>
2018-02-24 12:25:54 +0100

    Add webfontloader

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 // @flow
 import React from 'react'
 import ReactDOM from 'react-dom'
+import WebFont from 'webfontloader'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,500', 'Material Icons']
+  }
+})
 
 // $FlowFixMe
 ReactDOM.render(<App />, document.getElementById('root'))
