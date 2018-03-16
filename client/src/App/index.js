db491abec6901e293b4a6de22ae08d2c939f2b0f
ALEXANDRE Julien <22328659+ndrpnt@users.noreply.github.com>
2018-03-16 15:20:58 +0100

    Add react-helmet to dynamically update page head

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,23 +1,34 @@
 // @flow
 import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import CssBaseline from 'material-ui/CssBaseline'
 import Header from './Header/index'
 import Home from '../scenes/Home/index'
 import About from '../scenes/About/index'
 import Topics from '../scenes/Topics/index'
 
+// Page title & headerTitle should probably be managed using Redux ?
 const HomePage = () => (<Fragment>
+  <Helmet>
+    <title>AWS – Home</title>
+  </Helmet>
   <Header title='AWS – Home' />
   <Home />
 </Fragment>)
 
 const AboutPage = () => (<Fragment>
+  <Helmet>
+    <title>AWS – About</title>
+  </Helmet>
   <Header title='AWS – About' />
   <About />
 </Fragment>)
 
 const TopicsPage = (props) => (<Fragment>
+  <Helmet>
+    <title>AWS – Topics</title>
+  </Helmet>
   <Header title='AWS – Topics' />
   <Topics {...props} />
 </Fragment>)
