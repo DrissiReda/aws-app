62c7e466b76981548573b36897b2a6384fa98c3a
ALEXANDRE Julien <22328659+ndrpnt@users.noreply.github.com>
2018-03-16 10:32:29 +0100

    Add default values to drawer state

--- client/src/App/Header/reducers.js
+++ client/src/App/Header/reducers.js
@@ -1,7 +1,7 @@
 // @flow
 import { CLOSE_DRAWER, OPEN_DRAWER } from './constants'
 
-export const drawer = (state: Object = {}, action: Object) => {
+export const drawer = (state: Object = {isOpen: false}, action: Object) => {
   switch (action.type) {
     case OPEN_DRAWER:
       return {
