ce177c2c92cba1850e9ebc94412c80c283461e5a
ALEXANDRE Julien <22328659+ndrpnt@users.noreply.github.com>
2018-03-18 19:07:22 +0100

    Add a 404 page

--- client/src/scenes/App/index.js
+++ client/src/scenes/App/index.js
@@ -1,12 +1,13 @@
 // @flow
 import React, { Fragment } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 import CssBaseline from 'material-ui/CssBaseline'
 import Header from './components/Header/index'
 import SceneWrapper from './components/SceneWrapper/index'
 import Home from './scenes/Home/index'
 import Settings from './scenes/Settings/index'
+import Error from './scenes/404/index'
 
 // Page title & headerTitle should probably be managed using Redux ?
 const HomePage = () => (<Fragment>
@@ -29,10 +30,22 @@ const SettingsPage = () => (<Fragment>
   </SceneWrapper>
 </Fragment>)
 
+const ErrorPage = () => (<Fragment>
+  <Helmet>
+    <title>Error 404 (not found)</title>
+  </Helmet>
+  <Header title='AWS – 404' />
+  <SceneWrapper>
+    <Error />
+  </SceneWrapper>
+</Fragment>)
+
 export default () => (<Fragment>
   <CssBaseline />
   <Switch>
     <Route exact path='/' render={HomePage} />
-    <Route path='/settings' render={SettingsPage} />
+    <Route exact path='/settings' render={SettingsPage} />
+    <Route exact path='/404' render={ErrorPage} />
+    <Redirect to='/404' />
   </Switch>
 </Fragment>)
