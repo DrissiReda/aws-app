bae90adb72452ab485a8aff7ff84dc422099b112
ALEXANDRE Julien <22328659+ndrpnt@users.noreply.github.com>
2018-03-18 18:14:06 +0100

    Delete unused component

--- client/src/components/About/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react'
-
-const About = () => (<div>
-  <h2>About</h2>
-  1<br />2<br />3<br />4<br />5<br />6<br />7<br />8<br />9<br />10<br />11<br />12<br />13<br />14<br />15<br />16<br />18<br />19<br />20
-</div>)
-
-export default About
