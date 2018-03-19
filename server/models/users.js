34dad86439fe658834443915a8d64126aaacafed
mrnvr <marin.virlogeux@gmail.com>
2018-03-19 17:11:17 +0100

    Added models and controller New dependencies in package.json

---
+++ server/models/users.js
@@ -0,0 +1,57 @@
+// @flow
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const options = '_id username email profile_picture_url'
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    profile_picture_url: {
+      type: String,
+      default: 'https://www.shareicon.net/data/128x128/2016/09/02/824411_man_512x512.png'
+    }
+  }
+)
+
+let User = module.exports = mongoose.model('User', userSchema)
+
+// get all users -> probably not useful
+module.exports.getUsers = (callback, limit) => {
+  User.find({}, callback).limit(limit)
+}
+
+// get user by id
+// send the id username and email
+module.exports.getUserById = (id, callback) => {
+  User.findById(id, options, callback)
+}
+
+// get user by name
+// send the id username and email
+module.exports.getUserByName = (username, callback) => {
+  User.findOne({username: username}, options, callback) // return null if none!
+}
+
+// add new user to the DB
+module.exports.addUser = (user, callback) => {
+  User.create(user, callback)
+}
+
+// update infos
+module.exports.updateInfos = (callback) => {
+  User.update({}, {multi: true}, callback)
+}
---
+++ server/src/controller.js
@@ -0,0 +1,95 @@
+// @flow
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.listen(5000)
+
+const Users = require('../models/users')
+const Tweets = require('../models/tweets')
+
+mongoose.connect('mongodb://admin:istrator@ds111279.mlab.com:11279/aws-twitter-project', function (err) {
+  if (err) throw err
+})
+
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log('connected')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// send all tweets
+app.get('/api/tweets', (req, res) => {
+  Tweets.getTweets((err, tweets) => {
+    if (err) throw err
+    res.json(tweets)
+  })
+})
+
+// send all tweets by an user
+app.get('/api/tweets/:userId', (req, res) => {
+  Tweets.getTweetsByUser(req.params.userId, (err, tweets) => {
+    if (err) throw err
+    res.json(tweets)
+  })
+})
+
+// send all users
+app.get('/api/users', (req, res) => {
+  Users.getUsers((err, users) => {
+    if (err) throw err
+    res.json(users)
+  })
+})
+
+// send user matching the id
+app.get('/api/users/:_id', (req, res) => {
+  Users.getUserById(req.params._id, (err, user) => {
+    if (err) throw err
+    res.json(user)
+  })
+})
+
+// send user info for the name in the url
+app.get('/api/users/find/:username', (req, res) => {
+  Users.getUserByName(req.params.username, (err, user) => {
+    if (err) throw err
+    res.json(user)
+  })
+})
+
+// create new user
+app.post('/api/signup', (req, res) => {
+  let newUser = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  }
+  Users.addUser(newUser, (err, user) => {
+    if (err) throw err
+    res.json(user)
+  })
+})
+
+// create a new tweet
+app.post('/api/tweets', (req, res) => {
+  let tweet = req.body
+  Tweets.tweet(tweet, (err, tweet) => {
+    if (err) throw err
+    res.json(tweet)
+  })
+})
+
+app.get('/api/update', (req, res) => {
+  Users.updateInfos((err) => {
+    if (err) throw err
+    res.send('updated')
+  })
+})
